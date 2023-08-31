6ac421b948e57257544dd8b90337cfe5fa406a4a
Raushan Raj <raj.raushan9101@gmail.com>
2023-08-31 17:50:31 +0530

    integer input
    taking integer as input in C

---
+++ Basic/input.c
@@ -0,0 +1,9 @@
+//Taking integer as input in c programming 
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);      //it will ask user to input a number
+    printf("%d",n);      // it will print the number
+}
